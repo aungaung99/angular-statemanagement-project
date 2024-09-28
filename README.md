068ea7ff9e16a836d88abd9a17991f775103b6a8
Aung Naing Oo <aungaung20.ab@gmail.com>
2024-09-29 01:50:32 +0630

    inital project files

---
+++ README.md
@@ -0,0 +1,2 @@
+# angular-statemanagement-project
+ 
---
+++ src/app/app.config.ts
@@ -0,0 +1,16 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideStore } from '@ngxs/store';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideStore(
+      [],
+    )]
+};
